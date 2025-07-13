32a79bcedb701a929e24f70a77a4685777bb2fdb
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-07-13 07:03:26 +0545

    Enums in typescript

---
+++ Enums/myEnums.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeacrt = SeatChoice.AISLE;
