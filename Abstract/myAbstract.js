57ee291e35966d4322163a9f55623572d4874f97
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-07-13 07:01:03 +0545

    Added Abstract

---
+++ Abstract/myAbstract.js
@@ -0,0 +1,43 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var TakePhoto = /** @class */ (function () {
+    function TakePhoto(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    TakePhoto.prototype.getReelTime = function () {
+        return 8;
+    };
+    return TakePhoto;
+}());
+// const bhuvan = new TakePhoto("test","test") // Not allowed 
+var Instagram = /** @class */ (function (_super) {
+    __extends(Instagram, _super);
+    function Instagram(cameraMode, filter, brust) {
+        var _this = _super.call(this, cameraMode, filter) || this;
+        _this.cameraMode = cameraMode;
+        _this.filter = filter;
+        _this.brust = brust;
+        return _this;
+    }
+    Instagram.prototype.getSepian = function () {
+        console.log("sepian");
+    };
+    return Instagram;
+}(TakePhoto));
+var bhuvan = new Instagram("test", "Test", 3);
---
+++ Abstract/myAbstract.ts
@@ -0,0 +1,32 @@
+abstract class TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string
+    ) {}
+
+    abstract getSepian(): void
+    getReelTime(): number{
+        return 8
+    }
+}
+// const bhuvan = new TakePhoto("test","test") // Not allowed 
+
+class Instagram extends TakePhoto {
+
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public brust : number
+    ) { 
+        super(cameraMode, filter)
+    }
+    getSepian(): void {
+        console.log("sepian")
+    }
+
+}
+const bhuvan = new Instagram("test", "Test",3)
+
+export{}
+
+
