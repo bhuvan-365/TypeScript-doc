8378d6846363a18cbe240c07e3d03830dd708a02
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-07-14 06:43:35 +0545

    Tuples in Typescript

---
+++ Tuples/myTuples.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const User: (string | number)[] = [1, "bb"]
+var tUser;
+tUser = ["bb", 131, true]; // no reverse { order of array matters}
+var rgp = [255, 123, 234];
+var newUser = [112, "example@g.com"];
+newUser[1] = "hc.com";
---
+++ Tuples/myTuples.ts
@@ -0,0 +1,18 @@
+// const User: (string | number)[] = [1, "bb"]
+let tUser: [string,number,boolean]
+
+tUser=["bb" ,131,true]  // no reverse { order of array matters}
+
+let rgp:[number,number,number] = [255,123,234]
+
+type User = [number,string]
+
+
+
+const newUser: User =[112,"example@g.com"]
+
+newUser[1]= "hc.com"
+// newUser.push(true) *
+
+
+export{}
