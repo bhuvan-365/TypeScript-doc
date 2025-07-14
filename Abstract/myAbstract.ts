b89858b97478b1f19e92c7d40d1528cb6d100237
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-07-14 06:40:24 +0545

    fix in Abstarsct and Interface

--- Abstract/myAbstract.ts
+++ Abstract/myAbstract.ts
@@ -1,3 +1,4 @@
+
 abstract class TakePhoto {
     constructor(
         public cameraMode: string,
--- Interface/myInterface.ts
+++ Interface/myInterface.ts
@@ -30,4 +30,45 @@ const bhuvan: Adim = {
 }
 bhuvan.email = "h@h.com"
 // bhuvan.dbId = 45678;
+
+
+//CLass 2
+
+interface TakePhoto {
+    cameraMode: string,
+    filter: string,
+    brust: number,
+
+}
+interface Story {
+    createStory(): void
+}
+
+class Instagram implements TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public brust: number,
+    ) { }
+
+}
+class Youtube implements TakePhoto, Story {
+    constructor(
+
+        public cameraMode: string,
+        public filter: string,
+        public brust: number,
+        public shorts: string // you can add more but not less than TakePhoto 
+
+    ) { }
+    createStory(): void {
+        console.log("Story is created")
+    }
+
+}
+
+
+
+
+
 export { }
