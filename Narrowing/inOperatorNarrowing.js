39f2f8f9022b7ada79200737f25fe53bfb5d780f
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-08-14 10:52:31 +0545

    fix

--- Narrowing/inOperatorNarrowing.js
+++ Narrowing/inOperatorNarrowing.js
@@ -1,3 +1,6 @@
+// in operator narrowing: Checks if a property exists in an object, helping TypeScript narrow the type.
+// Example: if ("prop" in obj) { /* obj has prop */ }
+
 function isAdminAccount(account) {
     if ("isAdmin" in account) {
         return account.isAdmin;
