cfc578f284965e3729c3c94017ba068d4b678877
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-11-14 19:52:28 +0545

    Update Type_Predicates.ts

--- Type_Predicates.ts
+++ Type_Predicates.ts
@@ -1,15 +1,18 @@
 type Fish = { swim: () => void };
 type Bird = { fly: () => void };
 
+
+
 function isFish(pet: Fish | Bird): pet is Fish {// if true then pet is fish
-    return (pet as Fish).swim !== undefined
+    return (pet as Fish).swim !==
+
+ undefined
 }
 
 function getFood(pet: Fish | Bird) {
     if (isFish(pet)) {
         pet
         return "fish food"
-
     } else {
         pet
         return 'bird Food'
